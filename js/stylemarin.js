c1a968eccf36707e9a6c7cf8d9b218293dd37816
hakunagi <hakunagi.rei@gmail.com>
2021-01-01 00:59:09 +0900

    新年初コミット

--- js/stylemarin.js
+++ js/stylemarin.js
@@ -1,2 +1,11 @@
 // Stylemarin v0.0.1 | MIT License | github.com/Submarinonline/Stylemarin
-$("textarea").attr("rows", 1).on("input", e => $(e.target).height(0).innerHeight(e.target.scrollHeight));
+
+//textareaサイズ自動変更
+$("textarea").attr("rows", 1).on("input", e =>
+  $(e.target).height(0).innerHeight(e.target.scrollHeight)
+);
+
+//ボタンからフォーカスを外す
+$('button,select,input[type="submit"],input[type="button"],input[type="reset"]').click(() => {
+  document.activeElement.blur()
+});
